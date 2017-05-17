f85520ec4e2b53c45a813a93f3f74260519fe01a
leon in 't Veld <leon3110l@gmail.com>
2017-05-17 19:54:33 +0200

    improved updateDB function

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ function updateDB() {
                   // console.log(error, results, fields);
                 });
               }
-            }
+            });
           });
         }(i));
       }
